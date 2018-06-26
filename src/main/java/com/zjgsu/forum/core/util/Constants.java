275d14be66c8d7559c694e219e1339544985ae10
qianshu <qianshu@hikvision.com.cn>
2018-06-26 14:59:16 +0800

    [add]常量配置

---
+++ src/main/java/com/zjgsu/forum/core/util/Constants.java
@@ -0,0 +1,10 @@
+package com.zjgsu.forum.core.util;
+
+/**
+ * Created by qianshu on 2018/6/26.
+ */
+public class Constants {
+
+    public static final String COMMA = ",";
+    public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+}
