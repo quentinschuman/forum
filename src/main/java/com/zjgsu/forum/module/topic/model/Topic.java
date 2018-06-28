78f7e2d3b015acacac4b7ff730092c1846fdf3dd
qianshu <qianshu@hikvision.com.cn>
2018-06-28 14:13:06 +0800

    [add]collect comment log notification module

--- src/main/java/com/zjgsu/forum/module/topic/model/Topic.java
+++ src/main/java/com/zjgsu/forum/module/topic/model/Topic.java
@@ -1,5 +1,14 @@
 package com.zjgsu.forum.module.topic.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.zjgsu.forum.core.util.Constants;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * ProjectName: forum
@@ -7,5 +16,52 @@ package com.zjgsu.forum.module.topic.model;
  * Date: 2018/6/27
  * Time: 21:41
  */
-public class Topic {
+@Entity
+@Table(name = "forum_topic")
+@Getter
+@Setter
+public class Topic implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true,nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String content;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = Constants.DATETIME_FORMAT)
+    private Date inTime;
+
+    @JsonFormat(pattern = Constants.DATETIME_FORMAT)
+    @Column(name = "last_comment_time")
+    private Date lastCommentTime;
+
+    private Boolean top;
+    private Boolean good;
+
+    @Column(nullable = false)
+    private Integer view;
+
+    private Integer userId;
+
+    @Column(name = "comment_count")
+    private Integer commentCount;
+
+    private Integer up;
+    private Integer down;
+
+    @Column(columnDefinition = "text")
+    private String upIds;
+
+    @Column(columnDefinition = "text")
+    private String sownIds;
+
+    @Column(columnDefinition = "DOUBLE DEFAULT 0/0")
+    private Double weight;
+
+    private String tag;
 }
